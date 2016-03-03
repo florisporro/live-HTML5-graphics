21808400d887979036983d09b358c0d78a4a36ae
florisporro <floris@florisporro.com>
2016-03-03 12:30:54 +0100

    Menu icons reflect current state

--- public/js/admin.js
+++ public/js/admin.js
@@ -8,6 +8,15 @@ socket.on('cpu', function(cpu) {
 });
 
 socket.on('state', function(state) {
+	console.log(state);
+	for (var widget in state) {
+		if (state[widget].visibility != undefined) {
+			$('#'+widget).find('.eye').toggleClass('label-default', !state[widget].visibility);
+			$('#'+widget).find('.eye').toggleClass('label-success', state[widget].visibility);
+			$('#'+widget).find('.eye').find('i').toggleClass('glyphicon-eye-open', state[widget].visibility);
+			$('#'+widget).find('.eye').find('i').toggleClass('glyphicon-eye-close', !state[widget].visibility);
+		}
+	}
 });
 
 $(document).ready(function(){
