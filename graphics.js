bb2eb3073022433c7b6fd69390a54cfe6d7a3645
florisporro <floris@florisporro.com>
2016-07-12 18:03:46 +0200

    Animation overhaul

--- graphics.js
+++ graphics.js
@@ -5,26 +5,153 @@ var clock_offset = 0;
 var countdown_target = 0;
 
 socket.on('state', function (state) {
+	setTimeout(function(){
+		$('.widget').removeClass('hide');
+	}, 1000);
 
 	window.state = state;
 
 	console.log(state);
 
-	var widgets = ['broadcastMessage', 'clock', 'countdown', 'logo', 'twitter', 'lowerThirds'];
+	// var widgets = ['broadcastMessage', 'clock', 'countdown', 'logo', 'twitter', 'lowerThirds'];
+	var widgets = [
+		{
+			name: 'broadcastMessage',
+			transitionDirection: 'bottom'
+		},
+		{
+			name: 'clock',
+			transitionDirection: 'left'
+		},
+		{
+			name: 'countdown',
+			transitionDirection: 'left'
+		},
+		{
+			name: 'logo',
+			transitionDirection: 'right'
+		},
+		{
+			name: 'twitter',
+			transitionDirection: 'right'
+		},
+		{
+			name: 'lowerThirds',
+			transitionDirection: 'left'
+		},
+		];
+
+	// for (var i in widgets2) {
+	// 	console.log(widgets2[i].name);
+	// }
 
 	// Generic
 	for (var i in widgets) {
 		// Widget updating
-		$('#'+widgets[i]).toggleClass('hide', !state[widgets[i]].visibility);
+		if (!state[widgets[i].name].visibility) {
+			switch(state.general.animations) {
+				case 'fades':
+					$('#'+widgets[i].name).velocity({ opacity: 0 }, { duration: state.general.animation_length });
+					break;
+				case 'slides':
+					switch(widgets[i].transitionDirection){
+						case 'top':
+							$('#'+widgets[i].name).velocity({
+							    translateY: "-1080px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutQuint" });
+						break;
+						case 'right':
+							$('#'+widgets[i].name).velocity({
+							    translateX: "1920px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutQuint" });
+						break;
+						case 'bottom':
+							$('#'+widgets[i].name).velocity({
+							    translateY: "1080px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutQuint" });
+						break;
+						case 'left':
+							$('#'+widgets[i].name).velocity({
+							    translateX: "-1920px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutQuint" });
+						break;
+					}
+					break;
+				case 'perspective':
+					break;
+				case 'character':
+					break;
+				case 'bouncy':
+					switch(widgets[i].transitionDirection){
+						case 'top':
+							$('#'+widgets[i].name).velocity({
+							    translateY: "-1080px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutBack" });
+						break;
+						case 'right':
+							$('#'+widgets[i].name).velocity({
+							    translateX: "1920px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutBack" });
+						break;
+						case 'bottom':
+							$('#'+widgets[i].name).velocity({
+							    translateY: "1080px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutBack" });
+						break;
+						case 'left':
+							$('#'+widgets[i].name).velocity({
+							    translateX: "-1920px"
+							}, { duration: state.general.animation_length,
+								 easing: "easeInOutBack" });
+						break;
+					}
+					break;
+				default:
+			}
+		} else {
+			switch(state.general.animations) {
+				case 'fades':
+					$('#'+widgets[i].name).velocity({ opacity: 1 }, { duration: 1000 });
+					break;
+				case 'slides':
+					$('#'+widgets[i].name).velocity({
+					    translateY: "0px",
+					    translateX: "0px"
+					}, { duration: state.general.animation_length,
+						 easing: "easeInQuint" });
+					break;
+				case 'perspective':
+					break;
+				case 'character':
+					break;
+				case 'bouncy':
+					$('#'+widgets[i].name).velocity({
+					    translateX: "0",
+					    translateY: "0"
+					}, { duration: state.general.animation_length,
+						 easing: [500,30] });
+					break;
+				default:
+			}
+		}
+
+
+		// $('#'+widgets[i].name).toggleClass('hide', !state[widgets[i].name].visibility);
 
 		// General styling
-		$('#'+widgets[i]).toggleClass('font-techy', (state.general.fonts === 'techy'));
-		$('#'+widgets[i]).toggleClass('font-modern', (state.general.fonts === 'modern'));
-		$('#'+widgets[i]).toggleClass('font-classical', (state.general.fonts === 'classical'));
-		$('#'+widgets[i]).toggleClass('shadowed', (state.general.shadows === 'true'));
-		$('#'+widgets[i]).toggleClass('background-transparency', (state.general.transparent_elements === 'true'));
-		$('#'+widgets[i]).toggleClass('background-solid', (state.general.transparent_elements === 'false'));
-		$('#'+widgets[i]).toggleClass('animation-fades', (state.general.animations === 'fades'));
+		$('#'+widgets[i].name).toggleClass('font-techy', (state.general.fonts === 'techy'));
+		$('#'+widgets[i].name).toggleClass('font-modern', (state.general.fonts === 'modern'));
+		$('#'+widgets[i].name).toggleClass('font-classical', (state.general.fonts === 'classical'));
+		$('#'+widgets[i].name).toggleClass('shadowed', (state.general.shadows === 'true'));
+		$('#'+widgets[i].name).toggleClass('background-transparency', (state.general.transparent_elements === 'true'));
+		$('#'+widgets[i].name).toggleClass('background-solid', (state.general.transparent_elements === 'false'));
 		
 		// Primary Color
 		$('.widget').css('border-bottom-color', state.general.color1);
@@ -44,7 +171,7 @@ socket.on('state', function (state) {
 	if (state.broadcastMessage.visibility !== false) {
 		if (state.broadcastMessage.entries[state.broadcastMessage.visibility] === undefined) {
 			console.log('BroadcastMessage hidden because message id does not exist. Deleted whilst showing?');
-			$('#'+widgets[i]).text('');
+			$('#'+widgets[i].name).text('');
 		} else {
 			$('#broadcastMessage').text(state.broadcastMessage.entries[state.broadcastMessage.visibility].message);
 		}
@@ -54,7 +181,7 @@ socket.on('state', function (state) {
 	if (state.lowerThirds.visibility !== false) {
 		if (state.lowerThirds.entries[state.lowerThirds.visibility] === undefined) {
 			console.log('lowerThirds hidden because message id does not exist. Deleted whilst showing?');
-			$('#'+widgets[i]).text('');
+			$('#'+widgets[i].name).text('');
 		} else {
 			console.log(state.lowerThirds.entries[state.lowerThirds.visibility].name);
 			$('#lowerThirds #name').text(state.lowerThirds.entries[state.lowerThirds.visibility].name);
